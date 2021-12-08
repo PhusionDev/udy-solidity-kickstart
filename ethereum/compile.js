f2961f48dc0f0a088ecca714cfd378cb3fb4e44c
Jared Hodgkins <phusiondev@gmail.com>
2021-12-07 22:02:49 -0500

    147. More on Compile

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -8,10 +8,13 @@ fs.removeSync(buildPath);
 const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol");
 const source = fs.readFileSync(campaignPath, "utf8");
 const output = solc.compile(source, 1).contracts;
+
 fs.ensureDirSync(buildPath);
+
+console.log(output);
 for (let contract in output) {
   fs.outputJsonSync(
-    path.resolve(buildPath, contract + ".json"),
+    path.resolve(buildPath, contract.replace(":", "") + ".json"),
     output[contract]
   );
 }
