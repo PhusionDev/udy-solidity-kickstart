57f08e038289a82ec6b5451e8236f8051c5305ae
Jared Hodgkins <phusiondev@gmail.com>
2021-12-08 08:00:45 -0500

    152-153. Requiring Minimum Contributions, Array Getters

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -1,10 +1,10 @@
-const assert = require("assert");
-const ganache = require("ganache-cli");
-const Web3 = require("web3");
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-const compiledFactory = require("../ethereum/build/CampaignFactory.json");
-const compiledCampaign = require("../ethereum/build/Campaign.json");
+const compiledFactory = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
 
 let accounts;
 let factory;
@@ -16,11 +16,11 @@ beforeEach(async () => {
 
   factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data: compiledFactory.bytecode })
-    .send({ from: accounts[0], gas: "1000000" });
+    .send({ from: accounts[0], gas: '1000000' });
 
   await factory.methods
-    .createCampaign("100")
-    .send({ from: accounts[0], gas: "1000000" });
+    .createCampaign('100')
+    .send({ from: accounts[0], gas: '1000000' });
 
   // ES syntax to take first element from array
   [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
@@ -30,23 +30,42 @@ beforeEach(async () => {
   );
 });
 
-describe("Campaigns", () => {
-  it("deploys a factory contract", () => {
+describe('Campaigns', () => {
+  it('deploys a factory contract', () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
   });
 
-  it("marks caller as the campaign manager", async () => {
+  it('marks caller as the campaign manager', async () => {
     const manager = await campaign.methods.manager().call();
     assert.equal(accounts[0], manager);
   });
 
-  it("allows people to contribute money and marks them as approvers", async () => {
+  it('allows people to contribute money and marks them as approvers', async () => {
     await campaign.methods
       .contribute()
-      .send({ from: accounts[1], value: "200" });
+      .send({ from: accounts[1], value: '200' });
 
     const isContributor = await campaign.methods.approvers(accounts[1]).call();
     assert(isContributor);
+  });
+
+  it('requires a minimum contribution', async () => {
+    try {
+      await campaign.methods
+        .contribute()
+        .send({ from: accounts[1], value: '5' });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('allows a manager to make a payment request', async () => {
+    await campaign.methods
+      .createRequest('Buy Batteries', '100', accounts[1])
+      .send({ from: accounts[0], gas: '1000000' });
+    const request = await campaign.methods.requests(0).call();
+    assert.equal('Buy Batteries', request.description);
   });
 });
