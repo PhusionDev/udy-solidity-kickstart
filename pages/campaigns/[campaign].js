37185db241548decc97c0f2ed3549716d416b70d
Jared Hodgkins <phusiondev@gmail.com>
2021-12-09 22:05:07 -0500

    200. CampaignShow's getInitialProps

--- pages/campaigns/[campaign].js
+++ pages/campaigns/[campaign].js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import Layout from "../../components/layout";
 
 class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    console.log(props);
+    console.log(props.query.campaign);
+
+    return {};
+  }
+
   render() {
     return (
       <Layout>
