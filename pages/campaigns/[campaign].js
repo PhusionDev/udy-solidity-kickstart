4db98b767b193b1e91668bde05d3c5605a6a26c7
Jared Hodgkins <phusiondev@gmail.com>
2021-12-10 12:43:58 -0500

    203. Custom Card Groups

--- pages/campaigns/[campaign].js
+++ pages/campaigns/[campaign].js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Layout from '../../components/layout';
 import CampaignInstance from '../../ethereum/campaign';
+import { Card } from 'semantic-ui-react';
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -16,10 +17,34 @@ class CampaignShow extends Component {
     };
   }
 
+  renderCards() {
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      approversCount,
+    } = this.props;
+
+    const items = [
+      {
+        key: 0,
+        header: manager,
+        meta: 'Address of Manager',
+        description:
+          'The manager created this campaign and can create requests to withdraw money',
+        style: { overflowWrap: 'break-word' },
+      },
+    ];
+
+    return <Card.Group items={items} />;
+  }
+
   render() {
     return (
       <Layout>
         <h3>Campaign Show</h3>
+        {this.renderCards()}
       </Layout>
     );
   }
