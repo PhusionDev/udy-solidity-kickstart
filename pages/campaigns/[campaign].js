e4763f08fe425579fcb55c06c5cc7dcfdd2f1cee
Jared Hodgkins <phusiondev@gmail.com>
2021-12-09 22:33:49 -0500

    201. Accessing a Campaign

--- pages/campaigns/[campaign].js
+++ pages/campaigns/[campaign].js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import Layout from "../../components/layout";
+import CampaignInstance from "../../ethereum/campaign";
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
-    console.log(props);
-    console.log(props.query.campaign);
+    // console.log(props);
+    // console.log(props.query.campaign);
+    const campaign = CampaignInstance(props.query.campaign);
+    const summary = await campaign.methods.getSummary().call();
+
+    console.log(summary);
 
     return {};
   }
