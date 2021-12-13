34710922d7df69f9eacec6310edbcffa723b0805
Jared Hodgkins <phusiondev@gmail.com>
2021-12-13 09:50:13 -0500

    214. More Routing

--- pages/campaigns/[campaign]/requests.js
+++ pages/campaigns/[campaign]/requests.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import { Link } from '../../../routes';
 import Layout from '../../../components/layout';
 
 class RequestIndex extends Component {
+  static async getInitialProps(props) {
+    const { campaign } = props.query;
+
+    return { campaign };
+  }
+
   render() {
     return (
       <Layout>
-        <h3>Request List</h3>
+        <h3>Requests</h3>
+        <Link route={`/campaigns/${this.props.campaign}/requests/new`}>
+          <a>
+            <Button primary>Add Request</Button>
+          </a>
+        </Link>
       </Layout>
     );
   }
