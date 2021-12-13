faa31110500d692a57f2a869ab0b33cb30f49da8
Jared Hodgkins <phusiondev@gmail.com>
2021-12-13 12:22:06 -0500

    227. Finalizing Requests

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -13,6 +13,15 @@ class RequestRow extends Component {
       .send({ from: accounts[0] });
   };
 
+  onFinalize = async () => {
+    const campaign = CampaignInstance(this.props.address);
+    const accounts = await web3.eth.getAccounts();
+
+    await campaign.methods
+      .finalizeRequest(this.props.id)
+      .send({ from: accounts[0] });
+  };
+
   render() {
     const { Row, Cell } = Table;
     const { id, request, approversCount } = this.props;
@@ -29,6 +38,11 @@ class RequestRow extends Component {
         <Cell>
           <Button color='green' basic onClick={this.onApprove}>
             Approve
+          </Button>
+        </Cell>
+        <Cell>
+          <Button color='teal' basic onClick={this.onFinalize}>
+            Finalize
           </Button>
         </Cell>
       </Row>
