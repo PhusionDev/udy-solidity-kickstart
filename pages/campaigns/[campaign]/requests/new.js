35c68e30a9c664d55bbc35d9ec11dd7238ad8c4e
Jared Hodgkins <phusiondev@gmail.com>
2021-12-13 10:05:29 -0500

    215. Request Creation Form

--- pages/campaigns/[campaign]/requests/new.js
+++ pages/campaigns/[campaign]/requests/new.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
+import { Message, Button, Form, Input, Label } from 'semantic-ui-react';
+import CampaignInstance from '../../../../ethereum/campaign';
+import web3 from '../../../../ethereum/web3';
 import Layout from '../../../../components/layout';
+import { Link, Router } from '../../../../routes';
 
 class NewRequest extends Component {
+  state = {
+    value: '',
+    description: '',
+    recipient: '',
+  };
+
+  static async getInitialProps() {
+    const { campaign } = props.query;
+
+    return { campaign };
+  }
+
   render() {
     return (
       <Layout>
-        <h3>New Request</h3>
+        <h3>Create a Request</h3>
+        <Form>
+          <Form.Field>
+            <Label>Description</Label>
+            <Input
+              value={this.state.description}
+              onChange={(event) =>
+                this.setState({ description: event.target.value })
+              }
+            />
+          </Form.Field>
+          <Form.Field>
+            <Label>Value in Ether</Label>
+            <Input
+              value={this.state.value}
+              onChange={(event) => this.setState({ value: event.target.value })}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Label>Recipient</Label>
+            <Input
+              value={this.state.recipient}
+              onChange={(event) =>
+                this.setState({ recipient: event.target.value })
+              }
+            />
+          </Form.Field>
+
+          <Button primary>Create!</Button>
+        </Form>
       </Layout>
     );
   }
