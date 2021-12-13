64ce0cdad33b7f4727c2841af42755380326a9c8
Jared Hodgkins <phusiondev@gmail.com>
2021-12-13 11:49:33 -0500

    220. Fancy Javascript

--- pages/campaigns/[campaign]/requests.js
+++ pages/campaigns/[campaign]/requests.js
@@ -2,19 +2,32 @@ import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/layout';
+import CampaignInstance from '../../../ethereum/campaign';
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
-    const { campaign } = props.query;
+    const address = props.query.campaign;
+    const campaign = CampaignInstance(address);
+    const requestCount = await campaign.methods.getRequestCount().call();
 
-    return { campaign };
+    const requests = await Promise.all(
+      Array(requestCount)
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+
+    console.log(requests);
+
+    return { address, requests, requestCount };
   }
 
   render() {
     return (
       <Layout>
         <h3>Requests</h3>
-        <Link route={`/campaigns/${this.props.campaign}/requests/new`}>
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
           <a>
             <Button primary>Add Request</Button>
           </a>
